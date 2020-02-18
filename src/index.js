d6f33a2ebe539b076c4897a4528b15630d82fb81
Joby Joseph <joby.joseph@litmus7.com>
2020-02-18 10:46:55 +0530

    Initial draft

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return <h1>Hello React 16,Webpack 4 & Babel 7!</h1>;
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      } 
+    ]
+  },
+  plugins: [new HtmlWebpackPlugin({
+    template: './src/index.html'
+  })]
+};
